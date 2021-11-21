660811dba098c9f18ff80670fdc7750a70214a5f
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-11-21 22:28:14 +0600

    initial Commit

---
+++ stack.js
@@ -0,0 +1,27 @@
+class Stack {
+    constructor(){
+        this.stack = []
+    }
+    add(element){
+        this.stack.push(element)
+    }
+    remove(){
+        return this.stack.pop();
+    }
+}
+const names = new Stack();
+
+names.add('AAA');
+names.add('BBB');
+names.add('CCC');
+names.add('DDD');
+names.add('EEE');
+
+console.log(names.stack)
+
+names.remove();
+console.log(names.stack)
+names.remove();
+console.log(names.stack)
+names.remove();
+console.log(names.stack)
